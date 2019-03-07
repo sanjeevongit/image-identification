b9bced57628f6aabd007358a6beed79e18086e39
nicholasmfong <nicholasmfong@gmail.com>
2019-03-06 23:36:33 -0800

    Google labeler

--- google/labeler.py
+++ google/labeler.py
@@ -8,42 +8,71 @@ Created on Thu Feb 14 18:03:21 2019
 
 import io
 import os
-
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "./cloudcvauthentication.json"
-
+import pandas as pd
+import re
+import time
+import nltk
+nltk.download('wordnet')
 # Imports the Google Cloud client library
 # pip install google-cloud-vision
 from google.cloud import vision
 from google.cloud.vision import types
 
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = os.path.join(os.getcwd(), 'google_cloud_authentication.json')
 # Instantiates a client
 client = vision.ImageAnnotatorClient()
+lemmatizer = nltk.stem.WordNetLemmatizer()
 
-# The name of the image file to annotate
-file_name = os.path.join(
-    os.path.dirname(__file__),
-    'AGO Composite Photo.jpg')
+def extract_label(path):
+    m = re.search(r'(\\)(.+)(_)', path)
+    return m.group(2)
+def get_dataset(directory):
+    data = pd.DataFrame(columns=['path', 'class'])
+    for path, directories, files in os.walk(directory):
+        for file in files:
+            filename, file_extension = os.path.splitext(file)
+            if file_extension.strip().lower() == '.jpg':
+                label = lemmatizer.lemmatize(extract_label(path))
+                row = pd.DataFrame([[os.path.join(path, file), label]], columns=['path', 'class'])
+                data = data.append(row, ignore_index=True)
+    return data
 
-# Loads the image into memory
-with io.open(file_name, 'rb') as image_file:
-    content = image_file.read()
+data = get_dataset('../data/ImageNet')
+tags = pd.DataFrame(columns=['tag1', 'score1', 'tag2', 'score2', 'tag3', 'score3', 'tag4', 'score4', 'tag5', 'score5', 'success'])
+start_time = time.time()
 
-image = types.Image(content=content)
+for i in range(len(data)):
+    file_name = data.path[i]
+    # Loads the image into memory
+    with io.open(file_name, 'rb') as image_file:
+        content = image_file.read()
+    
+    image = types.Image(content=content)
+    
+    # Performs label detection on the image file
+    response = client.label_detection(image=image)
+    labels = response.label_annotations
+    tag_scores = []
+    success = 0
+    count = 0  # I know this is stupid, but this isn't worth refactoring
+    for label in labels:
+        tag = lemmatizer.lemmatize(label.description.lower())
+        score = label.score
+        tag_scores.append(tag)
+        tag_scores.append(label.score)
+        if tag == data['class'][i] and count < 5:
+            success = score
+        count += 1
+    tag_scores = tag_scores[:10]
+    while len(tag_scores) < 10:
+        tag_scores.append('')
+        tag_scores.append(0)
+    tag_scores = tag_scores[:10]
+    tag_scores.append(success)
+    row = pd.DataFrame([tag_scores], columns=['tag1', 'score1', 'tag2', 'score2', 'tag3', 'score3', 'tag4', 'score4', 'tag5', 'score5', 'success'])
+    tags = tags.append(row, ignore_index=True)
+end_time = time.time()
+print(end_time - start_time)
+results = data.join(tags)
 
-# Performs label detection on the image file
-response = client.label_detection(image=image)
-labels = response.label_annotations
-
-print('Labels:')
-for label in labels:
-    print(label.description)
-
-
-
-
-
-
-
-
-
-
+print('Mean Score:', sum(results.success) / len(results))
